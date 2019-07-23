2ac03ec86d4b7516f43b800ae1a478d125a379e6
VivekanandanPandiyan <Vivekanandan.Pandiyan@cognizant.com>
2019-07-23 00:58:09 -0700

    Commit

---
+++ TaskManagerService/Task.BusinessLayer/TaskManagerBL.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Task.DataAccessLayer;
+
+namespace Task.BusinessLayer
+{
+    public class TaskManagerBL
+    {
+        public Collection<TaskBL> GetTask()
+        {
+            using (var taskManager = new TaskManagerEntities())
+            {
+                Collection<TaskBL> taskCollection = new Collection<TaskBL>();
+
+                taskManager.tblTasks
+                   .Select(t => new TaskBL()
+                   {
+                       TaskID = t.TaskID,
+                       Task = t.Task,
+                       ParentTask = t.ParentTask,
+                       Priority = t.Priority,
+                       StartDate = t.StartDate,
+                       EndDate = t.EndDate,
+                       IsActive = t.IsActive
+                   })
+                   .ToList()
+                   .ForEach(y => taskCollection.Add(y));
+                return taskCollection;
+            }
+        }
+
+        public Collection<string> GetParentTasks()
+        {
+            using (var taskManager = new TaskManagerEntities())
+            {
+                Collection<string> taskCollection = new Collection<string>();
+
+                taskManager.tblTasks
+                   .Select(t => t.Task).ToList()
+                   .ForEach(y => taskCollection.Add(y));
+
+                return taskCollection;
+            }
+        }
+
+        public TaskBL GetTaskById(int taskId)
+        {
+            using (var taskManager = new TaskManagerEntities())
+            {
+                TaskBL task = new TaskBL();
+                task = taskManager.tblTasks
+                    .Where(x => x.TaskID == taskId)
+                    .Select(t => new TaskBL()
+                    {
+                        Task = t.Task,
+                        ParentTask = t.ParentTask,
+                        Priority = t.Priority,
+                        StartDate = t.StartDate,
+                        EndDate = t.EndDate,
+                        IsActive = t.IsActive
+                    }).FirstOrDefault();
+
+                return task;
+            }
+        }
+        public void AddTask(TaskBL task)
+        {
+            using (var taskManager = new TaskManagerEntities())
+            {
+                tblTask tTask = new tblTask
+                {
+                    Task = task.Task,
+                    ParentTask = task.ParentTask,
+                    Priority = task.Priority,
+                    StartDate = task.StartDate,
+                    EndDate = task.EndDate,
+                    IsActive = true
+                };
+
+                taskManager.tblTasks.Add(tTask);
+                taskManager.SaveChanges();
+            }
+        }
+
+        public void UpdateTask(TaskBL task)
+        {
+            using (var taskManager = new TaskManagerEntities())
+            {
+                var tTask = taskManager.tblTasks.Where(t => t.TaskID == task.TaskID).FirstOrDefault();
+                if (tTask != null)
+                {
+                    tTask.ParentTask = task.ParentTask;
+                    tTask.Priority = task.Priority;
+                    tTask.StartDate = task.StartDate;
+                    tTask.EndDate = task.EndDate;
+                    taskManager.SaveChanges();
+                }
+            }
+        }
+
+        public void EndTask(int taskId)
+        {
+            using (var taskManager = new TaskManagerEntities())
+            {
+                var task = taskManager.tblTasks.Where(t => t.TaskID == taskId).FirstOrDefault();
+                if (task != null)
+                {
+                    task.EndDate = DateTime.Now;
+                    task.IsActive = false;
+                    taskManager.SaveChanges();
+                }
+            }
+        }
+    }
+
+    public class TaskBL
+    {
+        public int TaskID { get; set; }
+        public string Task { get; set; }
+        public string ParentTask { get; set; }
+        public int Priority { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class SearchTask
+    {
+        public string Task { get; set; }
+        public string ParentTask { get; set; }
+        public int? PriorityFrom { get; set; }
+        public int? PriorityTo { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
---
+++ TaskManagerService/TaskManagerService/Controllers/TaskController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web.Http;
+using Task.BusinessLayer;
+using TaskManagerService.Models;
+
+namespace TaskManagerService.Controllers
+{
+    [RoutePrefix("api/Task")]
+    public class TaskController : ApiController
+    {
+        private TaskManagerBL taskManagerBL = null;
+        public TaskController()
+        {
+            taskManagerBL = new TaskManagerBL();
+        }
+
+        [HttpGet]
+        [Route("GetTasks")]
+        public IHttpActionResult GetTasks()
+        {
+            Collection<TaskModel> tasks = new Collection<TaskModel>();
+
+            var blTasks = taskManagerBL.GetTask();
+            blTasks.ToList().ForEach(
+               x => tasks.Add(
+                   new TaskModel
+                   {
+                       TaskID = x.TaskID,
+                       Task = x.Task,
+                       ParentTask = x.ParentTask ?? "",
+                       Priority = x.Priority,
+                       StartDate = x.StartDate,
+                       EndDate = x.EndDate,
+                       IsActive = x.IsActive
+                   }));
+
+            return Ok(tasks);
+        }
+
+        [HttpGet]
+        [Route("GetParentTasks")]
+        public IHttpActionResult GetParentTasks()
+        {
+            Collection<string> tasks = new Collection<string>();
+
+            var blTasks = taskManagerBL.GetParentTasks();
+            blTasks.ToList().ForEach(x => tasks.Add(x));
+
+            return Ok(tasks);
+        }
+
+        [HttpGet]
+        [Route("GetTasks/{taskId}")]
+        public IHttpActionResult GetTaskById(int taskId)
+        {
+            TaskModel task = new TaskModel();
+
+            var blTasks = taskManagerBL.GetTaskById(taskId);
+            if (blTasks != null)
+            {
+                task = new TaskModel
+                {
+                    TaskID = blTasks.TaskID,
+                    Task = blTasks.Task,
+                    ParentTask = blTasks.ParentTask,
+                    Priority = blTasks.Priority,
+                    StartDate = blTasks.StartDate,
+                    EndDate = blTasks.EndDate,
+                    IsActive = blTasks.IsActive
+                };
+            }
+            return Ok(task);
+        }
+
+        [HttpPost]
+        [Route("AddTask")]
+        public IHttpActionResult AddTask([FromBody]TaskModel task)
+        {
+            TaskBL blTask = new TaskBL
+            {
+                Task = task.Task,
+                ParentTask = task.ParentTask,
+                Priority = task.Priority,
+                StartDate = task.StartDate,
+                EndDate = task.EndDate
+            };
+            taskManagerBL.AddTask(blTask);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("UpdateTask")]
+        public void UpdateTask([FromBody]TaskModel task)
+        {
+            TaskBL blTask = new TaskBL
+            {
+                TaskID = task.TaskID,
+                ParentTask = task.ParentTask,
+                Priority = task.Priority,
+                StartDate = task.StartDate,
+                EndDate = task.EndDate
+            };
+
+            taskManagerBL.UpdateTask(blTask);
+        }
+
+        [HttpPost]
+        [Route("EndTask")]
+        public void EndTask([FromBody]int taskId)
+        {
+            taskManagerBL.EndTask(taskId);
+        }
+    }
+}
